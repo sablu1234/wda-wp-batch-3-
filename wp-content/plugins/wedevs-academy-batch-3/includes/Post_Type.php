b93fdd195b774e55f07688d2598903eb1201d9e7
sablu1234 <150502592+sablu1234@users.noreply.github.com>
2024-12-22 21:50:59 +0600

    22-12-2024

--- wp-content/plugins/wedevs-academy-batch-3/includes/Post_Type.php
+++ wp-content/plugins/wedevs-academy-batch-3/includes/Post_Type.php
@@ -16,6 +16,12 @@ class Post_Type {
         add_action( 'manage_book_posts_custom_column', array($this, 'manage_book_posts_custom_column'), 10, 2 );
         add_action( 'add_meta_boxes', array( $this, 'add_meta_boxes' ) );
         add_action( 'save_post_book', array( $this, 'save_post_book' ) );
+
+        if ( file_exists( AB_THREE_PLUGIN_PATH . 'lib/CMB2/init.php' ) ) {
+            require_once AB_THREE_PLUGIN_PATH . 'lib/CMB2/init.php';
+        }
+
+        add_action( 'cmb2_admin_init', array( $this, 'cmb2_admin_init' ) );
     }
 
     public function init () {
@@ -74,8 +80,22 @@ class Post_Type {
         }
 
         $terms = wp_get_post_terms( get_the_ID(), 'book_category');
-        print_r ($terms);
+        // print_r ($terms);
+        $another_title = get_post_meta( get_the_ID(), 'another_title', true );
+        $another_group = get_post_meta( get_the_ID(), 'another_title_group', true );
         ob_start();
+        
+        // print_r($another_group);
+        foreach( $another_group as $single_group ){
+            
+            if(isset($single_group['another_title_second'])){
+                // print_r($single_group['another_title_second']);
+                $vari = $single_group['another_title_second'];
+                foreach( $vari as $item ){
+                    echo $item;
+                }
+            }
+        }
 
         ?>
         <ul>
@@ -83,6 +103,7 @@ class Post_Type {
             <li><a href="<?php echo  get_term_link( $term, 'book_category' );?>"><?php echo $term->name; ?></a></li>
             <?php endforeach; ?>
         </ul>
+        <h3>Another Title:<?php echo $another_title; ?></h3>
         <?php
         $html = ob_get_clean();
 
@@ -140,6 +161,47 @@ class Post_Type {
         if (isset($_POST['book_subtitle'])){
             update_post_meta ( $post_id, 'book_subtitle', $_POST['book_subtitle'] );
         }
+    }
+
+    public function cmb2_admin_init () {
+        $box1 = new_cmb2_box( array(
+            'id' => 'custom-cmb2-box',
+            'title' => 'Custom CMB2 Box',
+            'object_types' => array( 'book' ),
+        ) );
+
+        $box1->add_field( array(
+            'id' => 'another_title',
+            'name' => 'Another Title',
+            'desc' => 'Enter Another Title',
+            'type' => 'text',
+        ) );
+
+        $group_field_id = $box1->add_field( array(
+            'id' => 'another_title_group',
+            'description' => 'Enter Another Title',
+            'type' => 'group',
+        ) );
+
+        $box1->add_group_field( $group_field_id,array(
+            'id' => 'another_title_second',
+            'name' => 'Another Second Title',
+            'desc' => 'Enter Second Another Title',
+            'type' => 'text',
+            'repeatable' => true,
+        ) );
+
+        $box1->add_group_field( $group_field_id,array(
+            'id' => 'another_select_second',
+            'name' => 'Another Select',
+            'desc' => 'Enter Second Select',
+            'type' => 'select',
+            'options' => array(1,2,3),
+        ) );
+
+
+
+
     }
 
 
