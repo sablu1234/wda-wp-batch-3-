8d55ff464fa32ca1e26d653d6fa7ab6cc9e39f3e
sablu1234 <150502592+sablu1234@users.noreply.github.com>
2024-12-24 22:57:14 +0600

    up

--- wp-content/plugins/Related-Posts/includes/related_data.php
+++ wp-content/plugins/Related-Posts/includes/related_data.php
@@ -1,34 +1,64 @@
-<?php 
+<?php
 namespace HASAN_RP;
 
-class related_post{
-    public function __construct()
-    {
-        add_filter( 'the_content', array( $this, 'the_content' ) );
+class Related_Post {
+
+
+    public function __construct() {
+        add_filter( 'the_content', [ $this, 'append_related_posts' ] );
     }
 
-    public function the_content () {
-        if (is_single() && get_post_type()==='post' ){
+    public function append_related_posts( $content ) {
+        if ( is_single() && get_post_type() === 'post' ) {
             global $post;
-            //query arguments
 
-            $args =[ 
-                'post_type' => 'post',
+            $args = [
+                'post_type'      => 'post',
                 'posts_per_page' => 5,
-               'post__not_in'   => [$post->ID],
-               'orderby'   => 'rand',
-               'tax_query'   => [
-                [
-                    'taxonomy' => 'category',
-                    'field' => 'term_id',
-                    'terms' => wp_get_post_categories( $post->ID ),
+                'post__not_in'   => [ $post->ID ],
+                'orderby'        => 'rand',
+                'tax_query'      => [
+                    [
+                        'taxonomy' => 'category',
+                        'field'    => 'term_id',
+                        'terms'    => wp_get_post_categories( $post->ID ),
+                    ],
                 ],
-               ],
-             ];
-             
-             //get related posts
+            ];
 
-             $related_posts = new \WP_Query( $args );
+            $related_posts = new \WP_Query( $args );
+
+            if ( $related_posts->have_posts() ) {
+                ob_start();
+
+                echo '<div class="related-posts">';
+                echo '<h3>Related Posts</h3>';
+                echo '<ul>';
+
+                while ( $related_posts->have_posts() ) {
+                    $related_posts->the_post();
+
+                    $post_title = get_the_title();
+                    $post_link = get_permalink();
+                    $post_image = get_the_post_thumbnail( get_the_ID(), 'thumbnail' );
+
+                    echo '<li>';
+                    if ( $post_image ) {
+                        echo '<a href="' . esc_url( $post_link ) . '">' . $post_image . '</a>';
+                    }
+                    echo '<a href="' . esc_url( $post_link ) . '">' . esc_html( $post_title ) . '</a>';
+                    echo '</li>';
+                }
+
+                echo '</ul>';
+                echo '</div>';
+
+                $content .= ob_get_clean();
+
+                wp_reset_postdata();
+            }
         }
+
+        return $content;
     }
 }
--- wp-content/plugins/Related-Posts/related-posts.php
+++ wp-content/plugins/Related-Posts/related-posts.php
@@ -3,48 +3,49 @@
 /*
  * Plugin Name:       Related Posts Plugin
  * Plugin URI:        https://Related-Posts
- * Description:       Show relative post on default view page.
- * Version:           .0.0.1
+ * Description:       Show related posts on the default view page.
+ * Version:           0.0.1
  * Requires at least: 5.2
  * Requires PHP:      7.2
- * Author:            sablu-hasan
+ * Author:            Sablu Hasan
  * Author URI:        https://www.facebook.com/md.doulot.3
  * License:           GPL v2 or later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Update URI:        https://www.facebook.com/md.doulot.3
  * Text Domain:       wda-batch-3
-*/
+ */
 
-
-if( ! defined( 'ABSPATH' )){
+if ( ! defined( 'ABSPATH' ) ) {
     return;
 }
 
-class hasan_related_post{
+class Hasan_Related_Post {
     private static $instance;
-//this is constructor
-    private function construct (){
+
+    private function __construct() {
         $this->define_constants();
-        $this->load_classe();
+        $this->load_classes();
     }
 
-    public static function get_instance (){
-        if( self::$instance ) {
+    public static function get_instance() {
+        if ( self::$instance ) {
             return self::$instance;
         }
 
         self::$instance = new self();
-
         return self::$instance;
     }
-//function define_constants
-    private function define_constants(){
-        define( 'HASAN_RELATIVE_PLUGIN_PATH', plugin_dir_path( __FILE__ )  );
+
+    private function define_constants() {
+        define( 'HASAN_RELATIVE_PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
     }
-    //for class loaded
-    private function load_classe () {
-        require_once HASAN_RELATIVE_PLUGIN_PATH . 'includes/related-posts.php';
+
+    private function load_classes() {
+        require_once HASAN_RELATIVE_PLUGIN_PATH . 'includes/related_data.php';
+
+          new HASAN_RP\Related_Post();
     }
 }
 
-hasan_related_post::get_instance();
+// Initialize the plugin
+Hasan_Related_Post::get_instance();
