ee75f8975883593ddce2ddeca2931ed13a41c373
sablu1234 <150502592+sablu1234@users.noreply.github.com>
2025-01-21 00:05:21 +0600

    up

--- wp-content/plugins/wp-ajax/assets/js/auth.js
+++ wp-content/plugins/wp-ajax/assets/js/auth.js
@@ -1,43 +1,16 @@
-jQuery(function($){
+jQuery(function ( $ ) {
 
-    $('form#profile-form').on('submit', function(e){
+    $( 'form#profile-form' ).on('submit', function (e){
         e.preventDefault();
 
-        var button = $(this).find('button[type="submit"]');
-        button.attr('disabled', 'disabled')
-        
         $.post(
-            simpleAuthAjax.ajax_url, 
-            $(this).serialize() + '&_wpnonce=' + simpleAuthAjax.nonce , 
+            simpleAuthAjax.ajax_url,
+            $(this).serialize() + '&_wpnonce' + simpleAuthAjax.nonce,
             function(response){
                 if(response.success){
-                    $('#profile-update-message')
-                        .html(response.data.message)
-                        .removeClass('hidden');
-
-                    setTimeout(function(){
-                        $('#profile-update-message').addClass('hidden');
-                    }, 4000);
-
-                    button.removeAttr('disabled');
-
+                    $('#profile-update-message').html(response.data.message);
                 }
             });
     })
 
-    $('form#simple-auth-login-form').on('submit', function(e){
-        e.preventDefault();
-
-        var button = $(this).find('button[type="submit"]');
-        button.attr('disabled', 'disabled')
-        
-        wp.ajax.post( 'simple-auth-login-form', $(this).serialize() )
-        .done( function(response){
-            console.log('success',response);
-        } )
-        .fail( function(error){
-            console.log( 'failed', error );
-        } );
-    }); 
-    
 });
